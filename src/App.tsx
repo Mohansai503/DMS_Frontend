e0dd905dcc0155f3655466eb47817634780e292b
Mohansai503 <mohansaiadavikayala@codestream.in>
2026-03-25 12:24:30 +0530

    updated code

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,12 @@ import RegSuccess from './components/RegSuccess';
 import RegFailure from './components/RegFailure';
 import LoginSuccess from './components/Loginsuccess';
 import LoginFailure from './components/LoginFailure';
-
+import MyLogin from './components/MyLogin';
 import DemoGreeting from './components/DemoLogin';
 import HomePage from './components/HomePage';
 
 
 import RecentPage from './components/RecentPage';
- 
- 
 
 
 
@@ -25,7 +23,9 @@ function App() {
       <Router>
         <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-indigo-50">
           <Routes>
+            
             <Route path='/reg' element={<Registration/>}></Route>
+            <Route path="/mylogin" element={<MyLogin />} />
             <Route path='/login' element={<Login/>}></Route>
             <Route path='/upload' element={<Upload/>}></Route>
             <Route path="/" element={<Navigate to="/login" replace />} />
@@ -34,10 +34,16 @@ function App() {
             <Route path='/loginsuccess' element={<LoginSuccess/>}></Route>
             <Route path='/loginfail' element={<LoginFailure/>}></Route>
 
+
             <Route path='/homepage' element={<HomePage/>}></Route>
            <Route path='/demologin' element={<DemoGreeting/>}></Route> 
             <Route path='/recent' element={<RecentPage/>}></Route>
            
+
+       
+            
+          
+
             
 
           </Routes>
